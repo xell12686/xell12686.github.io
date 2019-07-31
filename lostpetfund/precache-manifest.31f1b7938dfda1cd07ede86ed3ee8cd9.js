e2bcda70203037df446e4f2c2a11f582a0a12180
James FL <jlacandula@gmail.com>
2019-08-01 07:36:17 +0800

    LPF app production build initial commit

---
+++ lostpetfund/precache-manifest.31f1b7938dfda1cd07ede86ed3ee8cd9.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3c3b1304dcc9cd2d14da4503ea539bd",
+    "url": "/lostpetfund/index.html"
+  },
+  {
+    "revision": "01be9ba338c31ba4d98d",
+    "url": "/lostpetfund/static/css/main.c0181d9f.chunk.css"
+  },
+  {
+    "revision": "fbe7721dd8cc258acdb4",
+    "url": "/lostpetfund/static/js/2.cd2934a4.chunk.js"
+  },
+  {
+    "revision": "01be9ba338c31ba4d98d",
+    "url": "/lostpetfund/static/js/main.fdf68d30.chunk.js"
+  },
+  {
+    "revision": "ce8791113311cb1105fd",
+    "url": "/lostpetfund/static/js/runtime~main.d0221ab7.js"
+  },
+  {
+    "revision": "aec1ffa0bde5fa1fbfe63cd20ed54d82",
+    "url": "/lostpetfund/static/media/300.aec1ffa0.jpg"
+  },
+  {
+    "revision": "273cce891335345f9fe745ef3255d949",
+    "url": "/lostpetfund/static/media/350.273cce89.jpg"
+  },
+  {
+    "revision": "8c59e434c35b320aced30fc0f329ff84",
+    "url": "/lostpetfund/static/media/400.8c59e434.jpg"
+  },
+  {
+    "revision": "17712885a5309fe85a6665739ef6d9ff",
+    "url": "/lostpetfund/static/media/500.17712885.jpg"
+  },
+  {
+    "revision": "e68f94ad9715909e396cbe7b4ae17e88",
+    "url": "/lostpetfund/static/media/600.e68f94ad.jpg"
+  },
+  {
+    "revision": "e0ff028cdda5704b3ce3c053764e8310",
+    "url": "/lostpetfund/static/media/630.e0ff028c.jpg"
+  },
+  {
+    "revision": "f336a9ede5d91440032c0724cbbf4a94",
+    "url": "/lostpetfund/static/media/700.f336a9ed.jpg"
+  },
+  {
+    "revision": "422e766543a3850de8db1f049775a1e9",
+    "url": "/lostpetfund/static/media/fb.422e7665.svg"
+  },
+  {
+    "revision": "91a8e68dd3c119bb22f52aaff450c3b9",
+    "url": "/lostpetfund/static/media/ig.91a8e68d.svg"
+  },
+  {
+    "revision": "abbd65c2364504bd992a90ff2678f39d",
+    "url": "/lostpetfund/static/media/logo.abbd65c2.svg"
+  },
+  {
+    "revision": "88c38bdc6f8a183f915ded830d225ef3",
+    "url": "/lostpetfund/static/media/twitter.88c38bdc.svg"
+  },
+  {
+    "revision": "4b7620c7f6a73f7fe3d6dc88065f8b9f",
+    "url": "/lostpetfund/static/media/user.4b7620c7.svg"
+  },
+  {
+    "revision": "bc771c0090d5264f400a43734ee493ee",
+    "url": "/lostpetfund/static/media/youtube.bc771c00.svg"
+  }
+]);
