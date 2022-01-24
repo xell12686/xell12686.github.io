fa70d682f3c8d4ae69ddb76bf9dda1723b9c0e17
James L <xell12686@gmail.com>
2022-01-24 22:31:41 +0800

    add trickle-dashboard

---
+++ trickle-dashboard/precache-manifest.28babdffa6a271e1777db243b6fcf280.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ce7e6502a8775ebc0c526b03afe64a8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1bc9f5a97a1ed70e86fd",
+    "url": "/static/css/main.c73d1028.chunk.css"
+  },
+  {
+    "revision": "0caf41e47290b8c35022",
+    "url": "/static/js/2.550758da.chunk.js"
+  },
+  {
+    "revision": "1bc9f5a97a1ed70e86fd",
+    "url": "/static/js/main.8059cc02.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "be76611f7fa8cb1ffa28ae557d0b3202",
+    "url": "/static/media/Inter-Bold.be76611f.woff2"
+  },
+  {
+    "revision": "75cf3ef14619242444e7e71f01474a7b",
+    "url": "/static/media/Inter-Light-BETA.75cf3ef1.woff2"
+  },
+  {
+    "revision": "c6fde2a92fa6723f27876d1398672e99",
+    "url": "/static/media/Inter-Medium.c6fde2a9.woff2"
+  },
+  {
+    "revision": "47e70f6ff021c610674288ca517294f4",
+    "url": "/static/media/Inter-Regular.47e70f6f.woff2"
+  },
+  {
+    "revision": "de3f13e0e75b19919866ba61f72e64b1",
+    "url": "/static/media/Inter-SemiBold.de3f13e0.woff2"
+  },
+  {
+    "revision": "2bfdc99f05ff5fd6fd1027c4d9eef273",
+    "url": "/static/media/Inter-Thin-BETA.2bfdc99f.woff2"
+  },
+  {
+    "revision": "52d3add073c33f3d01057c0089407570",
+    "url": "/static/media/gif-img.52d3add0.png"
+  }
+]);
