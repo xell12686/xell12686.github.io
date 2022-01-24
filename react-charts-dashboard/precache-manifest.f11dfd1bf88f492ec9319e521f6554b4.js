07b32fedd71779d1f8472c355c3f139ea007d9a4
James L <xell12686@gmail.com>
2022-01-24 23:59:36 +0800

    removes sass-audition and adds react-graphs-dashboard

---
+++ react-charts-dashboard/precache-manifest.f11dfd1bf88f492ec9319e521f6554b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e658de7e1816022ed04b514d48a85b92",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d3a31830f11c6057dcfc",
+    "url": "/static/css/main.8769dff2.chunk.css"
+  },
+  {
+    "revision": "53c1c140a2b099c964b5",
+    "url": "/static/js/2.4b2cb637.chunk.js"
+  },
+  {
+    "revision": "61fb2e8d130dede9cb27bd885eb0f4f0",
+    "url": "/static/js/2.4b2cb637.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3a31830f11c6057dcfc",
+    "url": "/static/js/main.b8bf39a7.chunk.js"
+  },
+  {
+    "revision": "a0330bbd14cdcd7fb824",
+    "url": "/static/js/runtime-main.71156e0f.js"
+  }
+]);
